99ffd30d971bbff243950892abd543d86c1283ad
rinat715 <azat715@gmail.com>
2024-10-25 11:12:22 +0500

    edits

--- src/lib.rs
+++ src/lib.rs
@@ -1,29 +1,80 @@
 use nom::{
-    branch::alt,
-    bytes::complete::is_a,
-    character::complete::char,
-    IResult,
+    bytes::complete::{tag, take_until, take_while}, character::complete::multispace1, sequence::{delimited, tuple}, IResult, InputLength, InputTake, Compare,
+    error::ParseError
+
 };
 
 #[allow(dead_code)]
-fn delimeter(input: &str) -> IResult<&str, char> {
-    let (i, _) = is_a("-")(input)?;
-    let (a, b) = alt((char('['), char(']')))(i)?;
-    Ok((a, b))
+fn get_name(input: &str) -> IResult<&str, &str> {
+    let (i, _) = tag("Rule: ")(input)?;
+    take_until(" ]")(i)
 }
+
+
+pub fn get_name2<T, Input, Error: ParseError<Input>>(
+    start: T, stop: T
+  ) -> impl FnOnce(Input) -> IResult
+  where
+    Input: InputTake + Compare<T>,
+    T: InputLength + Clone,
+  {
+    move |i: Input| {
+        let (a, _) = tag(start)(i)?;
+        take_until(stop)(a)?;
+    };
+  }
+
+
+
+fn identity<T>(a: T) -> T {
+    return a;
+}
+
+fn right<T>(_a: T) -> impl Fn(T) -> T {
+    return identity;
+}
+
+
+
+fn get_value(start: &str, stop: &str) -> impl Fn(InputLength) -> IResult<&str, &str>
+{
+    let (i, _) = tag(start)(input)?;
+    take_until(stop)(i)
+}
+
+
+
+#[allow(dead_code)]
+fn name(input: &str) -> IResult<&str, &str> {
+    let left = (take_while(|c| c == '-'), tag("["), multispace1);
+    let right = (multispace1, tag("]"), take_while(|c| c == '-'));
+    
+
+    let mut parser = delimited(tuple(left), get_name, tuple(right));
+
+    parser(input)
+}
+
 
 #[cfg(test)]
 mod tests {
     use super::*;
 
     #[test]
-    fn delimeter_test() {
-        let arg = "--[";
-        let result = delimeter(arg);
-        assert_eq!(result, Ok(("", '[')));
+    fn name_test() {
+        // let arg = "Rule: First_test_rule";
+        let arg = "-------------[ Rule: First_test_rule ]--------------";
+        let result = name(arg);
+        assert_eq!(result, Ok(("", "First_test_rule")));
         
-        let arg = "--]";
-        let result = delimeter(arg);
-        assert_eq!(result, Ok(("", ']')));
+    }
+
+    #[test]
+    fn get_name_test() {
+        // let arg = "Rule: First_test_rule";
+        let arg = "Rule: First_test_rule ]--------------";
+        let result = get_name(arg);
+        assert_eq!(result, Ok((" ]--------------", "First_test_rule")));
+        
     }
 }
