ef5dac9816812c6bb163626f58c97f820d8f6acf
rinat715 <azat715@gmail.com>
2025-05-13 13:13:09 +0500

    edits

--- tester/macros/src/lib.rs
+++ tester/macros/src/lib.rs
@@ -1,8 +1,6 @@
 use proc_macro2::Span;
 use quote::quote;
 
-use syn::LitStr;
-
 use serde_derive::Deserialize;
 use std::env;
 use std::env::VarError;
@@ -12,12 +10,53 @@ use std::path::PathBuf;
 use toml::Table;
 use toml::Value;
 
-
 #[derive(Deserialize)]
 struct TestSuit {
     input: String,
     remaining: String,
     expected: Value,
+}
+
+impl TestSuit {
+    fn run_and_check_remaining(&self, func: &proc_macro2::Ident) -> proc_macro2::TokenStream {
+        let remaining = self.remaining.as_str();
+        let input = self.input.as_str();
+
+        quote! {
+            let (remaining, result) = #func(#input).unwrap();
+            assert_eq!(#remaining, remaining);
+        }
+    }
+    fn expected(&self) -> proc_macro2::TokenStream{
+        if self.expected.is_array() {
+            let expected = self
+            .expected
+            .as_array()
+            .unwrap()
+            .iter()
+            .map(|x| toml::to_string(x).unwrap());
+
+            quote! {
+                let mut index = 0;
+                for inner in vec![#(#expected),*] {
+                    println!("Run {}", index);
+                    assert_eq!(
+                        inner,
+                        toml::to_string(&result[index]).unwrap()
+                    );
+                    index += 1
+            }
+
+        } 
+    } else {
+        let expected_str = toml::to_string(&self.expected).unwrap();
+        let expected = expected_str.as_str();
+        quote! {
+            assert_eq!(#expected,  toml::to_string(&result).unwrap());
+        }
+    }
+        
+    }
 }
 
 fn fixture_dir() -> Result<PathBuf, VarError> {
@@ -38,8 +77,7 @@ pub fn tester(
     func: proc_macro::TokenStream,
 ) -> proc_macro::TokenStream {
     let func = syn::parse_macro_input!(func as syn::ItemFn);
-
-    let expr = syn::parse_str::<LitStr>(attrs.to_string().as_str()).unwrap();
+    let expr = syn::parse_macro_input!(attrs as syn::LitStr);
 
     let test_file = fixture_dir().unwrap().join(expr.value());
 
@@ -52,53 +90,29 @@ pub fn tester(
         println!("Run {}", test_name);
         let test: TestSuit = value.try_into().unwrap();
 
-        let remaining = test.remaining.as_str();
-        let input = test.input.as_str();
-
         let test_func = &func.sig.ident;
+        let run_and_check_remainder = test.run_and_check_remaining(test_func);
+        let expected = test.expected();
+
+        
         let mut f = test_func.to_string();
         f.insert(0, '_');
         f.insert_str(0, test_name.as_str());
         let test_name_ident = syn::Ident::new(f.as_str(), Span::call_site());
 
-        if test.expected.is_array() {
-            let expected= test.expected.as_array().unwrap().iter().map(|x|toml::to_string(x).unwrap());
-            let test = quote! {
-                #[test]
-                fn #test_name_ident() {
-                    let (remaining, results) = #test_func(#input).unwrap();
-                    assert_eq!(#remaining, remaining);
-                    let mut index = 0;
-                    for inner in vec![#(#expected),*] {
-                        println!("Run {}", index);
-                        assert_eq!(
-                            inner,
-                            toml::to_string(&results[index]).unwrap()
-                        );
-                        index += 1
-                    } 
-                }
-            };
-            vec.push(test);
-        } else {
-            let expected_str = toml::to_string(&test.expected).unwrap();
-            let expected = expected_str.as_str();
-
-            let test = quote! {
-                #[test]
-                fn #test_name_ident() {
-                    let (remaining, result) = #test_func(#input).unwrap();
-                    assert_eq!(#remaining, remaining);
-                    assert_eq!(#expected,  toml::to_string(&result).unwrap());
-                }
-            };
-            vec.push(test);
-        }
+        let test = quote! {
+            #[test]
+            fn #test_name_ident() {
+                #run_and_check_remainder
+                #expected
+            }
+        };
+         vec.push(test);
     }
 
     let res_res = quote! {
         #func
-         #(#vec)*
+        #(#vec)*
     };
     res_res.into()
 }
