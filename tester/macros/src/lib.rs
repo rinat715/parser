3b893a61eabfeaddc0d4fbd0afa85378814d0a02
rinat715 <azat715@gmail.com>
2025-05-14 18:31:07 +0500

    edits

--- tester/macros/src/lib.rs
+++ tester/macros/src/lib.rs
@@ -3,12 +3,14 @@ use quote::quote;
 
 use serde_derive::Deserialize;
 use std::env;
-use std::env::VarError;
 use std::fs::File;
 use std::io::prelude::*;
 use std::path::PathBuf;
 use toml::Table;
 use toml::Value;
+
+type Error = syn::Error;
+type Result<T> = syn::Result<T>;
 
 #[derive(Deserialize)]
 struct TestSuit {
@@ -18,57 +20,116 @@ struct TestSuit {
 }
 
 impl TestSuit {
-    fn run_and_check_remaining(&self, func: &proc_macro2::Ident) -> proc_macro2::TokenStream {
-        let remaining = self.remaining.as_str();
+    fn run(&self, func: &proc_macro2::Ident) -> proc_macro2::TokenStream {
         let input = self.input.as_str();
 
         quote! {
             let (remaining, result) = #func(#input).unwrap();
+        }
+    }
+    fn remainder(&self) -> proc_macro2::TokenStream {
+        let remaining = self.remaining.as_str();
+
+        quote! {
             assert_eq!(#remaining, remaining);
         }
     }
-    fn expected(&self) -> proc_macro2::TokenStream{
+    fn expected(&self) -> proc_macro2::TokenStream {
         if self.expected.is_array() {
             let expected = self
-            .expected
-            .as_array()
-            .unwrap()
-            .iter()
-            .map(|x| toml::to_string(x).unwrap());
+                .expected
+                .as_array()
+                .unwrap()
+                .iter()
+                .map(|x| toml::to_string(x).unwrap());
 
             quote! {
-                let mut index = 0;
-                for inner in vec![#(#expected),*] {
-                    println!("Run {}", index);
-                    assert_eq!(
-                        inner,
-                        toml::to_string(&result[index]).unwrap()
-                    );
-                    index += 1
-            }
+                    let mut index = 0;
+                    for inner in vec![#(#expected),*] {
+                        println!("Run {}", index);
+                        assert_eq!(
+                            inner,
+                            toml::to_string(&result[index]).unwrap()
+                        );
+                        index += 1
+                }
 
-        } 
-    } else {
-        let expected_str = toml::to_string(&self.expected).unwrap();
-        let expected = expected_str.as_str();
-        quote! {
-            assert_eq!(#expected,  toml::to_string(&result).unwrap());
+            }
+        } else {
+            let expected_str = toml::to_string(&self.expected).unwrap();
+            let expected = expected_str.as_str();
+            quote! {
+                assert_eq!(#expected,  toml::to_string(&result).unwrap());
+            }
         }
     }
-        
+    fn name(&self, func: &str, name: &str) -> syn::Ident {
+        let mut s = name.to_owned();
+        s.insert(0, '_');
+        s.insert_str(0, func);
+        syn::Ident::new(s.as_str(), Span::call_site())
+    }
+
+    fn build_test(&self, func: &proc_macro2::Ident, name: &str) -> proc_macro2::TokenStream {
+        let expected = self.expected();
+        let run = self.run(func);
+        let remainder = self.remainder();
+        let name = self.name(func.to_string().as_str(), name);
+
+        quote! {
+            #[test]
+            fn #name() {
+                #run
+                #remainder
+                #expected
+            }
+        }
     }
 }
 
-fn fixture_dir() -> Result<PathBuf, VarError> {
-    let path = PathBuf::new();
-    let manifest = env::var("CARGO_MANIFEST_DIR")?;
+struct TestFile();
 
-    Ok(path.join(manifest).join("tests").join("fixtures"))
-}
+impl TestFile {
+    fn new() -> Self {
+        Self {}
+    }
 
-fn read_file_to_string(path: &str, buf: &mut String) {
-    let mut f = File::open(path).unwrap();
-    f.read_to_string(buf).unwrap();
+    fn path(&self, name: &str) -> Result<PathBuf> {
+        let manifest = env::var("CARGO_MANIFEST_DIR").map_err(|e| {
+            Error::new_spanned(
+                "CARGO_MANIFEST_DIR",
+                format!("failed to resolve env var CARGO_MANIFEST_DIR: {e}"),
+            )
+        })?;
+
+        let path = PathBuf::new();
+        let file = path
+            .join(manifest)
+            .join("tests")
+            .join("fixtures")
+            .join(name);
+
+        Ok(file)
+    }
+
+    fn open(&self, path: PathBuf, buf: &mut String) -> Result<()> {
+        let mut f = File::open(path)
+            .map_err(|e| Error::new_spanned("ddfdfdf", format!("fail to open file: {e}")))?;
+
+        f.read_to_string(buf).map_err(|e| {
+            Error::new_spanned("CARGO_MANIFEST_DIR", format!("fail to read file: {e}"))
+        })?;
+
+        Ok(())
+    }
+
+    fn read(&self, name: &str) -> Result<String> {
+        let path = self.path(name)?;
+        let mut content = String::new();
+        let buf = &mut content;
+        self.open(path, buf)?;
+        Ok(content)
+    }
 }
 
 #[proc_macro_attribute]
@@ -79,35 +140,15 @@ pub fn tester(
     let func = syn::parse_macro_input!(func as syn::ItemFn);
     let expr = syn::parse_macro_input!(attrs as syn::LitStr);
 
-    let test_file = fixture_dir().unwrap().join(expr.value());
-
-    let mut buffer = String::new();
-    read_file_to_string(test_file.to_str().unwrap(), &mut buffer);
+    let file = TestFile::new().read(expr.value().as_str()).unwrap();
 
     let mut vec = Vec::new();
 
-    for (test_name, value) in buffer.parse::<Table>().unwrap() {
+    for (test_name, value) in file.parse::<Table>().unwrap() {
         println!("Run {}", test_name);
+
         let test: TestSuit = value.try_into().unwrap();
-
-        let test_func = &func.sig.ident;
-        let run_and_check_remainder = test.run_and_check_remaining(test_func);
-        let expected = test.expected();
-
-        
-        let mut f = test_func.to_string();
-        f.insert(0, '_');
-        f.insert_str(0, test_name.as_str());
-        let test_name_ident = syn::Ident::new(f.as_str(), Span::call_site());
-
-        let test = quote! {
-            #[test]
-            fn #test_name_ident() {
-                #run_and_check_remainder
-                #expected
-            }
-        };
-         vec.push(test);
+        vec.push(test.build_test(&func.sig.ident, test_name.as_str()));
     }
 
     let res_res = quote! {
